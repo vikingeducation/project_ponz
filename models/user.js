8fdae7bc4260fba7c8d2b337c20a9d41e49933de
DHail <dhail@ymail.com>
2017-04-26 08:31:25 -0700

    working on data structure

--- models/user.js
+++ models/user.js
@@ -4,10 +4,37 @@ var mongoose = require("mongoose"),
 
 var User = new Schema({
   referralCode: { type: String, unique: true },
-  refTree: [],
-  points: []
+  parentId: {type: Integer},
+  children: {type: Array}
 });
 
 User.plugin(passportLocalMongoose, { usernameField: "email" });
 
 module.exports = mongoose.model("User", User);
+
+
+
+
+Parent id: 17
+  [{1:23 2:43 3:56}, {1:73}]
+
+  Child-1 id: 23
+    [{1:43 2:56}, {1:74}]
+
+    Child-1-1 id: 43
+    [{1:56}]
+
+      Child-1-1-1 id:56
+
+    Child-1-2 id: 74
+      [{}]
+
+  Child-2 id: 73
+    [{}]
+
+User: 17 (23, 73)
+
+  User: 23 (43, 74)
+
+    User: 43 (56)
+
