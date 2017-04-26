41c2057e66719e9cad4ffb35607203bda9866095
William Whitworth <william10charles@gmail.com>
2017-04-26 11:31:55 -0600

    working on point distrobution

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,59 @@
 const express = require("express");
 let router = express.Router();
+const {
+  loggedInOnly,
+  loggedOutOnly
+} = require("../services/session");
 
 const User = require("../models/User");
 
 module.exports = passport => {
-
-  router.get("/", (req, res, next) => {
+  router.get("/", loggedInOnly, (req, res, next) => {
     const user = req.user;
     res.render("index", { user });
   });
 
-  router.get("/login", (req, res) => {
+  router.get("/login", loggedOutOnly, (req, res) => {
     res.render("login");
   });
 
-  router.get("/register", (req, res) => {
+  router.get("/register", loggedOutOnly, (req, res) => {
     res.render("register");
   });
 
-  router.get("/logout", (req, res) => {
+  router.get("/logout", loggedInOnly, (req, res) => {
     req.logout();
     res.redirect("/login");
   });
 
   router.post(
     "/login",
+    loggedOutOnly,
     passport.authenticate("local", {
       successRedirect: "/",
       failureRedirect: "/login"
     })
   );
 
-  router.post("/register", (req, res, next) => {
+  router.post("/register", loggedOutOnly, async (req, res, next) => {
     const { fname, lname, email, password } = req.body;
     const user = new User({
       fname: fname,
       lname: lname,
       email: email,
       points: 0,
-      depth: 0,
       password: password
     });
-    user.save()
-      .then(user => {
-        req.login(user, err => {
-          if (err) {
-            throw err;
-          }
-          res.redirect("/");
-        });
-      })
-      .catch(next);
+    let pointArray = [40, 20, 10, 5, 2, 1];
+    let counter = 0;
+    await user.save();
+    while (user.parent) {
+      let parent = await User.findById(user.parent);
+      parent.points += pointArray[counter];
+      if (counter < 5) {
+        counter++;
+      }
+    }
   });
 
   return router;
--- routes/ponzvert.js
+++ routes/ponzvert.js
@@ -1,11 +1,39 @@
 const express = require("express");
 let router = express.Router();
+const {
+  loggedInOnly,
+  loggedOutOnly
+} = require("../services/session");
 
 const User = require("../models/User");
 
-router.get('/:referralId', (req, res) => {
+router.get("/:referralId", loggedOutOnly, (req, res) => {
   const referralId = req.params.referralId;
   res.render("ponzvert", { referralId });
+});
+
+router.post("/", loggedOutOnly, (req, res, next) => {
+  const { fname, lname, email, password, referralId } = req.body;
+  const user = new User({
+    fname: fname,
+    lname: lname,
+    email: email,
+    points: 0,
+    depth: 0,
+    password: password,
+    parent: referralId
+  });
+  user
+    .save()
+    .then(user => {
+      req.login(user, err => {
+        if (err) {
+          throw err;
+        }
+        res.redirect("/");
+      });
+    })
+    .catch(next);
 });
 
 module.exports = router;
