5649eee5c3d9e0c0d42b9c61a7974dcb320c3621
Kara Thrash <dvdonato@gmail.com>
2017-08-16 11:21:46 -0400

    broken

--- app.js
+++ app.js
@@ -31,6 +31,35 @@ app.get("/", loggedInOnly, (req, res) => {
 app.get("/login", loggedOutOnly, (req, res) => {
   res.render("login");
 });
+app.post("/register/:id", loggedOutOnly, (req, res) => {
+  let newUser = {};
+  newUser.username = req.body.username;
+  newUser.password = req.body.password;
+  newUser.referrals = [];
+  if (req.params.id) {
+    newUser.referrerID = req.params.id;
+  }
+
+  User.create(newUser).then(() => {
+    return res.redirect("/");
+  });
+
+  res.render("login");
+});
+app.post("/login", loggedOutOnly, (req, res) => {
+  User.findOne({ username: req.body.username }).then(foundUser => {
+    if (foundUser === undefined) {
+      return res.redirect("/login");
+    }
+    if (foundUser.validatePassword(req.body.password) === true) {
+      const sessionId = createSignedSessionId(email);
+      res.cookie("sessionId", sessionId);
+      return res.redirect("/");
+    } else {
+      return res.redirect("/login");
+    }
+  });
+});
 
 app.listen(3000, "0.0.0.0", (req, res) => {
   console.log("listening on port 3000");
