7613aa09390d14de94b6a6609789ab4644ddab48
Andrew Senner <me@andrewsenner.com>
2017-08-16 17:06:07 -0400

    switch over

--- helpers/index.js
+++ helpers/index.js
@@ -1,27 +1,43 @@
 module.exports = {
-  makeTreeGraphStructure: function(user) {
-    let chartConfig = {
-      chart: {
-        container: "#tree"
-      },
-      nodeStructure: _createTreeGraphStructure(user)
-    };
-    return chartConfig;
-  }
+	makeTreeGraphStructure: function(user) {
+		let chartConfig = {
+			chart: {
+				container: '#tree',
+				//animateOnInit: true,
+				connectors: {
+					type: 'bCurve',
+					style: {
+						stroke: 'blue',
+						'stroke-width': '2px'
+					},
+					stackIndent: 2
+				},
+				node: {
+					collapsable: true
+				}
+			},
+			nodeStructure: _createTreeGraphStructure(user)
+		};
+		return chartConfig;
+	}
 };
 function _createTreeGraphStructure(user) {
-  let newObj = {
-    text: { name: user.username }
-  };
-  newObj.children = user.ponverts.map(child => {
-    return _createTreeGraphStructure(child);
-  });
-  return newObj;
+	let newObj = {
+		text: {
+			name: user.username
+		},
+		HTMLclass: 'panel panel-info tree-node'
+	};
+	newObj.children = user.ponverts.map(child => {
+		return _createTreeGraphStructure(child);
+	});
+	return newObj;
 }
 /*
 simple_chart_config = {
     chart: {
         container: "#tree-simple"
+
     },
 
     nodeStructure: {
--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,38 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const passport = require("passport");
-const { makeTreeGraphStructure } = require("../helpers");
-const { User } = require("../models");
+const passport = require('passport');
+const { makeTreeGraphStructure } = require('../helpers');
+const { User } = require('../models');
 
 /* GET home page. */
-router.get("/", async function(req, res, next) {
-  // working
-  const user = await User.findRecursive({ _id: req.user._id });
-  const treeGraph = makeTreeGraphStructure(user);
-  if (!user) {
-    return next(new Error("User not found..."));
-  }
-  console.log("treeGraph = ", treeGraph);
-  res.render("index", {
-    user: user,
-    userString: JSON.stringify(user, null, 2),
-    treeGraph: treeGraph
-  });
+router.get('/', async function(req, res, next) {
+	// working
+	const user = await User.findRecursive({ _id: req.user._id });
+
+	if (!user) {
+		return next(new Error('User not found...'));
+	}
+	const treeGraph = res.render('index', {
+		user: user,
+		treeGraph: JSON.stringify(makeTreeGraphStructure(user))
+	});
 });
 
-router.get("/login", (req, res) => {
-  return res.render("login");
+router.get('/login', (req, res) => {
+	return res.render('login');
 });
 
 router.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true
-  })
+	'/login',
+	passport.authenticate('local', {
+		successRedirect: '/',
+		failureRedirect: '/login',
+		failureFlash: true
+	})
 );
 
-router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/login");
+router.get('/logout', (req, res) => {
+	req.logout();
+	res.redirect('/login');
 });
 module.exports = router;
