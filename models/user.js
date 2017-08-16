57b4c85b0f2edd84beec3cd957df36ad0f75c8f4
Stephanie Barker <song_mage@yahoo.com>
2017-08-16 10:58:36 -0400

    still working on undoing my horrible mistake

---
+++ models/user.js
@@ -0,0 +1,49 @@
+// User
+// 	username
+// 	passwordHash
+// 	ponzBucks
+// 	parents: [nearest first]
+// 	children: [direct children as reference]
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const UserSchema = new Schema(
+  {
+    username: {type: String, required: true, unique: true},
+    passwordHash: {type: String, required: true},
+    ponzBucks: Number
+    parents: [{
+    	type: Schema.Types.ObjectId,
+      ref: "User"
+    }],
+    children: [{
+    	type: Schema.Types.ObjectId,
+      ref: "User"
+    }]
+  },
+  {
+    timestamps: true
+  }
+);
+
+UserSchema.plugin(uniqueValidator);
+
+UserSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.passwordHash);
+};
+
+UserSchema.virtual("password")
+  .get(function() {
+    return this._password;
+  })
+  .set(function(value) {
+    this._password = value;
+    this.passwordHash = bcrypt.hashSync(value, 12);
+  });
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
