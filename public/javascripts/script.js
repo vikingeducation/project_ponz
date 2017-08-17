82b0150e38a7ce280c77461671694d49588851eb
Andrew Senner <me@andrewsenner.com>
2017-08-17 18:50:32 -0400

    working

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,93 +1,92 @@
-const userId = getCookie("userId");
+const userId = getCookie('userId');
 
 //send events
-const REGENERATE = "regenerate";
-const ADD_NODE = "addNode";
-const LOAD_TREE = "loadTree";
-const GET_MORE_USERS = "getMoreUsers";
+const REGENERATE = 'regenerate';
+const ADD_NODE = 'addNode';
+const LOAD_TREE = 'loadTree';
+const GET_MORE_USERS = 'getMoreUsers';
 
 //receive events
-const NEW_TREE = "newTree";
-const NEW_NODE = "newNode";
-const DISPLAY_TREE = "displayTree";
-const UPDATE_POINTS = "updatePoints";
+const NEW_TREE = 'newTree';
+const NEW_NODE = 'newNode';
+const DISPLAY_TREE = 'displayTree';
+const UPDATE_POINTS = 'updatePoints';
 
 $(function() {
-  const socket = io.connect("http://localhost:3000");
+	const socket = io.connect('http://localhost:3000');
 
-  //load this user's tree
-  socket.emit(LOAD_TREE, userId);
-  socket.on(DISPLAY_TREE, data => {
-    //test
-    treeGraph = new Treant(data, function() {}, $);
-  });
-  socket.on(NEW_TREE, data => {
-    const chartConfig = {
-      chart: {
-        container: "#tree",
-        rootOrientation: "WEST",
-        levelSeparation: 50,
-        connectors: {
-          type: "bCurve",
-          style: {
-            stroke: "blue",
-            "stroke-width": "2px"
-          }
-        },
-        node: {
-          collapsable: true
-        },
-        animation: {
-          nodeSpeed: 50,
-          connectorsSpeed: 50
-        }
-      },
-      nodeStructure: data.node
-    };
-    treeGraph = new Treant(chartConfig, function() {}, $);
-  });
+	//load this user's tree
+	socket.emit(LOAD_TREE, userId);
+	socket.on(DISPLAY_TREE, data => {
+		console.log(data.chart);
+		console.log(data.nodeStructure);
+		treeGraph = new Treant(data, function() {}, $);
+	});
+	socket.on(NEW_TREE, data => {
+		const chartConfig = {
+			chart: {
+				container: '#tree',
+				rootOrientation: 'WEST',
+				levelSeparation: 50,
+				connectors: {
+					type: 'bCurve',
+					style: {
+						stroke: 'blue',
+						'stroke-width': '2px'
+					}
+				},
+				node: {
+					collapsable: true
+				},
+				animation: {
+					nodeSpeed: 50,
+					connectorsSpeed: 50
+				}
+			},
+			nodeStructure: data.node
+		};
+		treeGraph = new Treant(chartConfig, function() {}, $);
+	});
 
-  socket.on(NEW_NODE, async data => {
-    const id = "#" + data.parent._id;
-    const $parent = $(id);
+	socket.on(NEW_NODE, async data => {
+		const id = '#' + data.parent._id;
+		const $parent = $(id);
 
-    const parentNode = $parent.data("treenode");
-    treeGraph.addNewNode(parentNode, data.node);
-  });
+		const parentNode = $parent.data('treenode');
+		treeGraph.addNewNode(parentNode, data.node);
+	});
 
-  socket.on(UPDATE_POINTS, data => {
-    let { id, points, cash } = data;
-    if (data.root === true) {
-      $(`#ponz-points`).text(points);
-      $(`#ponz-cash`).text(cash);
-    }
-    $(`div#${id} p.node-title`).text(points);
-  });
+	socket.on(UPDATE_POINTS, data => {
+		let { id, points, cash } = data;
+		if (data.root === true) {
+			$(`#ponz-points`).text(points);
+			$(`#ponz-cash`).text(cash);
+		}
+		$(`div#${id} p.node-title`).text(points);
+	});
 
-  // Set form listener.
-  $("#generate-button").on("click", function(e) {
-    e.preventDefault();
-    let $form = $("form");
+	// Set form listener.
+	$('#generate-button').on('click', function(e) {
+		e.preventDefault();
+		let $form = $('form');
 
-    let [min, max, depth] = [
-      $("#minInput").val(),
-      $("#maxInput").val(),
-      $("#depthInput").val()
-    ];
+		let [min, max, depth] = [
+			$('#minInput').val(),
+			$('#maxInput').val(),
+			$('#depthInput').val()
+		];
 
-    // Emit the event.
-    socket.emit(REGENERATE, { userId: userId, data: { min, max, depth } });
-  });
+		// Emit the event.
+		socket.emit(REGENERATE, { userId: userId, data: { min, max, depth } });
+	});
 
-  // socket.emit(ADD_NODE,);
-
-  Treant.prototype.addNewNode = function(parent, nodeDef) {
-    return this.tree.addNode(parent, nodeDef);
-  };
+	Treant.prototype.addNewNode = function(parent, nodeDef) {
+		return this.tree.addNode(parent, nodeDef);
+	};
 });
 
 function getCookie(name) {
-  var value = "; " + document.cookie;
-  var parts = value.split("; " + name + "=");
-  if (parts.length == 2) return parts.pop().split(";").shift();
+	var value = '; ' + document.cookie;
+	var parts = value.split('; ' + name + '=');
+	if (parts.length == 2) return parts.pop().split(';').shift();
 }
