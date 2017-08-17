784bc6216deb763305e9e0547df1956bf0862269
Will Timpson <will.timpson@gmail.com>
2017-08-17 11:01:38 -0400

    remove mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
 const h = require("../helpers");
 const { User } = require("../models");
 
