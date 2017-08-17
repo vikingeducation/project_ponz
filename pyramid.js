02f3d52dcb8534711e1b2b01e8be9bac9a13c68a
Kara Thrash <dvdonato@gmail.com>
2017-08-17 13:19:00 -0400

    async problems math works

--- pyramid.js
+++ pyramid.js
@@ -16,37 +16,37 @@ const pyramid = async function(currentUserId) {
   makeObject(referralArray);
   // }
   // keepLooping = false;
-  // return pyramidObject;
+  return pyramidObject;
 };
 
 const makeObject = async function(refArray) {
   let newArray = [];
   pyramidObject[depth] = refArray;
-  console.log("before");
+  //  console.log("before");
   depth++;
   Promise.all(
     refArray.map(async function(i) {
       let user = await User.findOne({ _id: i._id }).populate("referrals");
-      console.log("inside of the promise.all array");
+      //  console.log("inside of the promise.all array");
       return user.referrals;
     })
   ).then(resultArray => {
     let objects = [];
-    console.log("inside the then from promise all");
+    //console.log("inside the then from promise all");
     resultArray.forEach(el => {
       objects = objects.concat(el);
     });
     pyramidObject[depth] = objects;
 
     if (objects.length > 0) {
-      console.log("this is objects");
-      console.log(objects);
-      console.log(pyramidObject);
-      depth++;
+      // console.log("this is objects");
+      // console.log(objects);
+      // console.log(pyramidObject);
+      //depth++;
       referralArray = objects;
       makeObject(objects);
     } else {
-      console.log("done");
+      //  console.log("done");
       depth = 0;
       keepLooping = false;
     }
@@ -61,6 +61,7 @@ const makeObject = async function(refArray) {
   // }
 
   // console.log("after each");
+  console.log(pyramidObject);
 };
 
 module.exports = pyramid;
