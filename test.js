2a9255d6cfd35639162081e0f58b8aa3e73262b9
Hai Nguyen <memyselfandhai@gmail.com>
2017-12-13 16:13:00 -0600

    re pushing

--- test.js
+++ test.js
@@ -1,2 +1,15 @@
 hello;
 
+{{#each currentUser.childIds as |ponzvert| }}
+  <!-- <tr> -->
+    <!-- <td>
+      40 points: <strong>{{ponzvert.displayName}} </strong> <em>ponzverted on {{ponzvert.createdAt}}</em>
+    </td> -->
+    {{>referral ponzvert=ponzvert}}
+  <!-- </tr> -->
+  <tr>
+    <!-- <td>
+      <span class="tab">20 points: <strong>{{ponzvert.displayName}} </strong> <em>ponzverted on {{ponzvert.createdAt}}</em></span>
+    </td> -->
+  </tr>
+{{/each }}
