ce8459af25ef12350f1d89748ed7e676c1f38779
GeneTinderholm <tinderholmgene@gmail.com>
2017-12-13 20:33:19 -0600

    unshift -> push

--- routers/home.js
+++ routers/home.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const router = express.Router();
-const User = require('./../models/User');
-const mongoose = require('mongoose');
-const passport = require('passport');
+const User = require("./../models/User");
+const mongoose = require("mongoose");
+const passport = require("passport");
 
 // 1
 const findTree = async function(user, distance) {
   let usrArr = await User.find({ referrer: user._id });
-  console.log('user', user, 'userid', user._id);
+  console.log("user", user, "userid", user._id);
   console.log(usrArr);
   if (usrArr === []) {
     return [user.username, distance];
@@ -18,10 +18,10 @@ const findTree = async function(user, distance) {
     for (let i = 0; i < usrArr.length; i++) {
       temp = await findTree(usrArr[i], distance + 1);
       temp.map(x => {
-        recursiveArray.unshift(x);
+        recursiveArray.push(x);
       });
     }
-    console.log('========================RECURSIVEARRAY', recursiveArray);
+    console.log("========================RECURSIVEARRAY", recursiveArray);
     // let recursiveArray = await usrArr.map(async function(x) {
     //   let y = await findTree(x, distance + 1);
     //   return y;
@@ -54,7 +54,7 @@ const calcPoints = distance => {
   }
 };
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   let user;
   let referrer;
   let userArray;
@@ -65,12 +65,12 @@ router.get('/', async (req, res) => {
     userArray = await findTree(user, 0);
     userArray.shift();
     console.log(
-      '---------------------------------------------------------------------',
+      "---------------------------------------------------------------------",
       userArray
     );
 
     userArray.forEach(
-      (x, i) => (userArray[i][2] = '   '.repeat(userArray[i][1]))
+      (x, i) => (userArray[i][2] = "   ".repeat(userArray[i][1]))
     );
 
     userArray.forEach((x, i) => (userArray[i][1] = calcPoints(x[1])));
@@ -88,37 +88,37 @@ router.get('/', async (req, res) => {
     //   })
   } finally {
     if (req.user) {
-      res.render('home', { referrer, user, userArray, sum });
+      res.render("home", { referrer, user, userArray, sum });
     } else {
-      res.redirect('/login');
+      res.redirect("/login");
     }
   }
 });
 
 // 2
-router.get('/login', (req, res) => {
-  res.render('login');
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
-router.get('/register', (req, res) => {
-  res.render('register');
+router.get("/register", (req, res) => {
+  res.render("register");
 });
 
-router.get('/register/:id', (req, res) => {
-  res.render('register');
+router.get("/register/:id", (req, res) => {
+  res.render("register");
 });
 
 // 3
 router.post(
-  '/login',
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
     failureFlash: true
   })
 );
 
-router.post('/register/:id', async (req, res, next) => {
+router.post("/register/:id", async (req, res, next) => {
   let { username, password } = req.body;
   let currentUser = new User({ username, password });
   currentUser.referrer = req.params.id;
@@ -127,13 +127,13 @@ router.post('/register/:id', async (req, res, next) => {
       if (err) {
         return next(err);
       }
-      return res.redirect('/');
+      return res.redirect("/");
     });
   });
 });
 
 // 4
-router.post('/register', (req, res, next) => {
+router.post("/register", (req, res, next) => {
   const { username, password } = req.body;
   const user = new User({ username, password });
   user.save((err, user) => {
@@ -141,15 +141,15 @@ router.post('/register', (req, res, next) => {
       if (err) {
         return next(err);
       }
-      return res.redirect('/');
+      return res.redirect("/");
     });
   });
 });
 
 // 5
-router.get('/logout', function(req, res) {
+router.get("/logout", function(req, res) {
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 });
 
 module.exports = router;
